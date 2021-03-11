a86908eea163a8d508339c967fcedd6402920461
Maciej Lyskawinski <maciej.lyskawinski@skillandchill.com>
2021-03-11 11:34:57 +0100

    Fixed typing in the constructor

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-export type Constructor<T = {}> = new (...args: any[]) => T;
+export type Constructor<T = any> = new (...args: any[]) => T;
 
 export function staticImplements<T>() {
     return <U extends T>(constructor: U) => constructor;
